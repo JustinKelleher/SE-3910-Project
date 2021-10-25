bf47c7118efdc0ef27f64a20da5f7f6d268df286
Justin Kelleher <10236186+JustinKelleher@users.noreply.github.com>
2021-10-25 16:46:18 -0400

    Initial commit

---
+++ src/main/java/com/example/commerce/MvcConfig.java
@@ -0,0 +1,19 @@
+package com.example.commerce;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("index");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/navbar").setViewName("navbar");
+        registry.addViewController("/admin").setViewName("admin");
+    }
+
+}
